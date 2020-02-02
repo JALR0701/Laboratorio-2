1eb0e4f03c1b1b8caca89b2960b3fd7d651ae229
JALR0701 <44651220+JALR0701@users.noreply.github.com>
2020-02-01 22:15:43 -0600

    Primera iteración de comentarios

--- Laboratorio_2.X/ADC.c
+++ Laboratorio_2.X/ADC.c
@@ -32,24 +32,24 @@
 #include <stdint.h>
 #define _XTAL_FREQ 4000000
 
-char change = 0;
-unsigned char const SEGMENT_MAP[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0X39, 0X5E, 0X79, 0X71};
+char change = 0; //Bandera para multiplexar
+unsigned char const SEGMENT_MAP[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0X39, 0X5E, 0X79, 0X71}; //Mapeo de valores en hexadecimal para el display 7 segmentos.
 
 void __interrupt() ISR (void){
-    INTCONbits.GIE = 0;
+    INTCONbits.GIE = 0; //deshabilitación de interrupciones
     INTCONbits.RBIE = 0;
     INTCONbits.T0IE = 0;
-    if(INTCONbits.T0IF == 1){
-        TMR0 = 4;
-        change = 1;
+    if(INTCONbits.T0IF == 1){ // interrupción del timer0
+        TMR0 = 4; //Define el valor precagado del timer0
+        change = 1; //Activa el cambio de transistor
     }
-    if(INTCONbits.RBIF == 1 && PORTBbits.RB0 == 1){
+    if(INTCONbits.RBIF == 1 && PORTBbits.RB0 == 1){ //interrupción del puerto b
         PORTA++;
     }
     if(INTCONbits.RBIF == 1 && PORTBbits.RB1 == 1){
         PORTA--;
     }
-    INTCONbits.GIE = 1;
+    INTCONbits.GIE = 1; //Reinicio de banderas y habilitación de interrupciones
     INTCONbits.RBIE = 1;
     INTCONbits.RBIF = 0;
     INTCONbits.T0IE = 1;
@@ -57,38 +57,38 @@ void __interrupt() ISR (void){
 }
 
 void main(void) {
-    TRISA = 0;
+    TRISA = 0; //I/O de puertos
     TRISB = 0;
     TRISBbits.TRISB0 = 1;
     TRISBbits.TRISB1 = 1;
     TRISC = 0;
     TRISD = 0;
     
-    ANSEL = 0;
+    ANSEL = 0; //Pines digitales
     ANSELH = 0;
     
-    INTCON = 0;
+    INTCON = 0; //Habilitación de interrupciones y reinicio de banderas
     INTCONbits.GIE = 1;
     INTCONbits.RBIE = 1;
     INTCONbits.RBIF = 0;
     INTCONbits.T0IE = 1;
     INTCONbits.T0IF = 0;
     
-    OPTION_REG = 0;
+    OPTION_REG = 0; //configuracion para desborde del timer 0
 
-    TMR0 = 4;
+    TMR0 = 4; //Precarga del timer 0
     
-    IOCB = 0;
+    IOCB = 0; //Bits del puerto B que provocan interrupciones
     IOCBbits.IOCB0 = 1;
     IOCBbits.IOCB1 = 1;
     
-    PORTA = 0;
+    PORTA = 0; //valor inicial de los puertos
     PORTB = 0;
     PORTC = 0;
     PORTD = 0;
     
-    while (1){
-        if(change == 1){
+    while (1){ //Loop
+        if(change == 1){ //Cambio de transistor para cada interrupcion del timer 0
             if(PORTDbits.RD0 == 1){
                 PORTDbits.RD0 = 0;
                 PORTDbits.RD1 = 0;
@@ -100,7 +100,7 @@ void main(void) {
                 PORTC = SEGMENT_MAP[11];
                 PORTDbits.RD0 = 1;
             }
-            change = 0;
+            change = 0; //Reinicio de bandera.
         }
         
     }
